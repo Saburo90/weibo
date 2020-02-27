8d344f03d75c9d12d499fa32496715e674117f17
root <saburo90@163.com>
2020-02-28 00:54:55 +0800

    基础页面

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // 代码提示工具
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
