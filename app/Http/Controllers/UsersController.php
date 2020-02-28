229797a213a9cc4c9adf160df78a8ca943bb3c21
root <saburo90@163.com>
2020-02-28 22:32:04 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,11 +18,23 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
+        // 注册用户信息后端验证
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        // 用户信息入库
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        // 缓存登录成功提示消息
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        // 登录成功重定向至个人中心页
+        return redirect()->route('users.show', [$user]);
     }
 }
