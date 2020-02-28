6ef761aaba32fd3d103200fc88049a3827239269
root <saburo90@163.com>
2020-02-28 21:47:08 +0800

    增加csrf验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,7 +9,7 @@
       </div>
       <div class="card-body">
         <form method="POST" action="{{ route('users.store') }}">
-
+          {{ csrf_token() }}
 
           <div class="form-group">
             <label for="name">名称：</label>
