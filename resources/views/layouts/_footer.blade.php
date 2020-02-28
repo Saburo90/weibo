7f53cdd6bd13dfd662687bee70cea617ab49019c
root <saburo90@163.com>
2020-02-28 16:36:01 +0800

    切割头部和底部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <img src="/images/logo.png" alt="" class="brand-icon">
+  <a href="https://github.com/Saburo90/weibo" target="_blank">
+    刻意练习，每日精进
+  </a>
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
