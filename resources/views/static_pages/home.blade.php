6620cc4d9b51a435fee26e075678ec5125383450
root <saburo90@163.com>
2020-02-28 15:38:30 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello, Laravel!</h1>
+    <p class="lead">
+      您现在所看到的是 <a href="https://github.com/Saburo90/weibo">Laravel 入门教程 - Weibo App</a> 的示例项目主页
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-success btn-lg">立即注册</a>
+    </p>
+  </div>
 @stop
