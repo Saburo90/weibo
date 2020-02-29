435ef4409bc814eff6c2f78e518f7309dc1f0dcb
root <saburo90@163.com>
2020-02-29 23:25:52 +0800

    线上邮件内容localhost问题解决

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://saburo-weibo.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
