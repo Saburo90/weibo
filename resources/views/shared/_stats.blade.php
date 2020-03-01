4a387760041d3a94af11322c6ab79612d6d3b2f8
root <saburo90@163.com>
2020-03-01 20:43:49 +0800

    社交信息统计

--- resources/views/shared/_stats.blade.php
+++ resources/views/shared/_stats.blade.php
@@ -1,4 +1,4 @@
-{{--<a href="{{ route('users.followings', $user->id) }}">
+<a href="{{ route('users.followings', $user->id) }}">
   <strong id="following" class="stat">
     {{ count($user->followings) }}
   </strong>
@@ -9,7 +9,7 @@
     {{ count($user->followers) }}
   </strong>
   粉丝
-</a>--}}
+</a>
 <a href="{{ route('users.show', $user->id) }}">
   <strong id="statuses" class="stat">
     {{ $user->statuses()->count() }}
