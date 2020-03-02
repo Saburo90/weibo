df95da9a644bc5e0e7749041a1692dbb6c3c5400
root <saburo90@163.com>
2020-03-02 15:52:25 +0800

    用户关注与取消关注

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -39,4 +39,16 @@ class UserPolicy
     {
         return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
+
+    /**
+     * @param User $currentUser
+     * @param User $user
+     * @return bool
+     * 用户关注鉴权
+     */
+    public function follow(User $currentUser, User $user)
+    {
+        // 用户自己不可关注自己
+        return $currentUser->id !== $user->id;
+    }
 }
